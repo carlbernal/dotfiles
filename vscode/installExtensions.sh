d7dec7a7322055c62b7d71fd92e222ca46367739
bernalcarl24 <carlbernal24@gmail.com>
2020-07-27 06:06:17 +0800

    Update vscode extensions

--- vscode/installExtensions.sh
+++ vscode/installExtensions.sh
@@ -10,4 +10,5 @@ code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension ms-python.python
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension PKief.material-icon-theme
+code --install-extension trixnz.vscode-lua
 code --install-extension vscodevim.vim
