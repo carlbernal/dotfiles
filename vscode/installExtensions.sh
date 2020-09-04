6b775f425685d32f33931e40408a2b4591ca26a1
bernalcarl24 <carlbernal24@gmail.com>
2020-09-04 22:14:42 +0800

    Export new extensions

--- vscode/installExtensions.sh
+++ vscode/installExtensions.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+code --install-extension bungcip.better-toml
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
-code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
+code --install-extension golang.go
 code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension ms-python.python
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension PKief.material-icon-theme
+code --install-extension svelte.svelte-vscode
 code --install-extension vscodevim.vim
