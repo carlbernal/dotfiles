8f2ab7c1498f025529f21bb020b2be197f010227
carlbernal <carlbernal24@gmail.com>
2021-11-15 23:50:59 +0800

    Clean up

--- vscode/install-vscode-ext.sh
+++ vscode/install-vscode-ext.sh
@@ -2,12 +2,13 @@
 
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
-code --install-extension dbaeumer.jshint
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
 code --install-extension eseom.nunjucks-template
 code --install-extension formulahendry.auto-rename-tag
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
 code --install-extension Pivotal.vscode-spring-boot
 code --install-extension PKief.material-icon-theme
 code --install-extension redhat.java
