77e76707accd5aeb1f7c19eafda6c1446d4dfc17
carlbernal <carlbernal24@gmail.com>
2022-01-20 10:55:09 +0800

    Add headless android studio instructions

--- vscode/install-vscode-ext.sh
+++ vscode/install-vscode-ext.sh
@@ -2,11 +2,10 @@
 
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
+code --install-extension Dart-Code.dart-code
+code --install-extension Dart-Code.flutter
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
-code --install-extension eseom.nunjucks-template
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension llvm-vs-code-extensions.vscode-clangd
 code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
 code --install-extension Pivotal.vscode-spring-boot
