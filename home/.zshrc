173231ecb589ff5009c0d9d0b7841856a1159a28
carlbernal <carlbernal24@gmail.com>
2022-02-01 12:19:29 +0800

    Clean up

--- home/.zshrc
+++ home/.zshrc
@@ -11,25 +11,10 @@ plugins=( \
 )
 
 source $ZSH/oh-my-zsh.sh
-
-# disable control + s in terminal
-stty -ixon
-
-# aliases
 source ~/.aliases
 
-# python3
 export PATH=$HOME/.local/bin:$PATH
-
-# android
-export PATH=$HOME/flutter/bin:$PATH
-export PATH=$HOME/Android/cmdline-tools/latest/bin:$PATH
-export ANDROID_SDK_ROOT=$HOME/Android
-
-# personal bin
-export PATH=$HOME/bin:$PATH
 export PATH=$HOME/scripts:$PATH
 
-# node
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
--- vscode/install-vscode-ext.sh
+++ vscode/install-vscode-ext.sh
@@ -2,14 +2,13 @@
 
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
-code --install-extension Dart-Code.dart-code
-code --install-extension Dart-Code.flutter
+# code --install-extension Dart-Code.dart-code
+# code --install-extension Dart-Code.flutter
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
-code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
-code --install-extension Pivotal.vscode-spring-boot
+# code --install-extension Pivotal.vscode-spring-boot
 code --install-extension PKief.material-icon-theme
-code --install-extension redhat.java
+# code --install-extension redhat.java
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscodevim.vim
