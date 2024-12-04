4a8d1631d9362a244e68ed8c8c8a15dea3b05bf6
carlbernal <carlbernal24@gmail.com>
2024-12-04 22:19:34 +0800

    Remove mock files in .scripts directory

--- .scripts/test.lua
+++
@@ -1,5 +0,0 @@
--- TODO hello hello hello hello hello hello
-
-function todoPrintHello()
-	print("hello world")
-end
--- .scripts/test.py
+++
@@ -1,9 +0,0 @@
-# todo hello hello hello hello hello hello
-
-
-def todoPrintHello():
-    """
-    TODo: fix this
-    [x] fix this
-    """
-    print("hello world")
